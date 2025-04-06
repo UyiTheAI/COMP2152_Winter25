d8e413b10681b8703fe3377d2c54a87373772803
Omoruyi Oredia <omoruyioredia@gmail.com>
2025-04-06 18:50:33 -0400

    Origin

---
+++ Project/hero.py
@@ -0,0 +1,12 @@
+import random
+from character import Character
+
+class Hero(Character):
+    def __init__(self):
+        super().__init__()
+
+    def __del__(hero):
+        print("The Hero object is being destroyed by the garbage collector")
+
+    def hero_attacks(hero, monster):
+       return hero.hero_attacks(monster)
---
+++ Project/monster.py
@@ -0,0 +1,13 @@
+import random
+from character import Character
+
+class Monster(Character):
+    def __init__(self):
+        super().__init__()
+
+
+    def __del__(monster):
+        print("The Monster object is being destroyed by the garbage collector")   
+             
+    def monster_attacks(monster, hero):
+       return monster.monster_attacks(hero)
